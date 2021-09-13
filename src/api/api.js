75b180dd97166288b0df92dd548c0ccf4a3a7095
Jacky Tea <teajacks@outlook.com>
2021-09-12 21:15:18 -0400

    api change

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 // GET request to NASA's APOD API
 export const getData = async (queryParams = "") => {
   try {
-    const response = await fetch(`${process.env.REACT_APP_API}${queryParams}`, { mode: 'cors' });
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=XPrDo4Mv7M893BJVqEwTm81Y9v24lFEncCpN10cV${queryParams}`, { mode: 'cors' });
     const data = await response.json();
     return data;
   } catch (error) {
