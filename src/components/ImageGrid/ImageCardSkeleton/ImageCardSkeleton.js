7917a5570f4e5418c4e942129d18c61e7750f613
Jacky Tea <teajacks@outlook.com>
2021-09-13 09:49:47 -0400

    fix loading skeleton width on mobile

--- src/components/ImageGrid/ImageCardSkeleton/ImageCardSkeleton.js
+++ src/components/ImageGrid/ImageCardSkeleton/ImageCardSkeleton.js
@@ -4,7 +4,7 @@ const ImageCardSkeleton = () => {
   return (
     <>
       {[...Array(6).keys()].map(index => (
-        <div key={index} className="animate-pulse w-96 bg-gray-300 rounded-lg shadow-lg dark:bg-gray-900 mt-4">
+        <div key={index} className="animate-pulse w-72 sm:w-96 bg-gray-300 rounded-lg shadow-lg dark:bg-gray-900 mt-4">
         <div className="animate-pulse flex space-x-4">
           <div className="p-4 max-w-sm w-full mx-auto">
             <div className="flex-1 space-y-4 py-1">
